fb33f73ad38d69c042400eae718fab2e5f541f40
Nuthelk <nuthelkcd@gmail.com>
2022-11-24 22:24:24 -0500

    fix

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import useCotizador from '../hooks/useCotizador'
 import {MARCAS, PLANES} from '../constants'
 
